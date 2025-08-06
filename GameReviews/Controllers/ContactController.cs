7bcfe5da985df9fb9d11a9ce0ca25b0713b22eef
YoanGurev <yoangurev0@gmail.com>
2025-08-06 17:58:19 +0300

    Added a contact controller and a contact form model for a "Contact us" action

---
+++ GameReviews/Controllers/ContactController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GameReviews.Models;
+
+public class ContactController : Controller
+{
+    public IActionResult Index() => View();
+
+    [HttpPost]
+    public IActionResult Send(ContactForm model)
+    {
+        if (!ModelState.IsValid)
+            return View("Index", model);
+
+        TempData["SuccessMessage"] = "Your message has been sent!";
+        return RedirectToAction("Index");
+    }
+}
+
